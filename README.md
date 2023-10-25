3c35bbed7ea48051fa59d8091fb92fb2936bc92e
edvince <2456510228@qq.com>
2023-10-25 13:52:48 +0800

    0.00041

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 2. 0.00023分：kernel+memcpy和sort overlap
 3. 0.00027分：calloc替换new+memeset，openmp并行化convert的for，碎片malloc合并到convert线程
 4. 0.00035分：最后的超长topk sort用多线程分块实现，最后再跑一次sort合并
+5. 0.00041分：用双stream把kernel和memcpy给overlap起来，总的是kernel、memcpy、sort三个overlap
 
 ## CPU优化(不算分)
 1. 多线程分块读取大txt文件
--- src/topk.cu
+++ src/topk.cu
@@ -60,10 +60,12 @@ void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &querys,
     ) {
 
     auto n_docs = docs.size();
-    float *d_scores = nullptr;
+    float* scores[2] = {nullptr,nullptr};
+    float *d_scores[2] = {nullptr,nullptr};
     uint16_t *d_docs = nullptr;
     uint16_t *d_doc_lens = nullptr;
     uint16_t *d_query = nullptr;
+    cudaStream_t kernelStream, memcpyStream;
 
     // cuda第一次启动要创建context，很慢且无法避免
 
@@ -90,7 +92,8 @@ void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &querys,
 
         cudaMalloc(&d_doc_lens, sizeof(uint16_t) * n_docs);
         cudaMalloc(&d_query, sizeof(uint16_t) * MAX_QUERY_SIZE);
-        cudaMalloc(&d_scores, sizeof(float) * n_docs);
+        cudaMalloc(&d_scores[0], sizeof(float) * n_docs);
+        cudaMalloc(&d_scores[1], sizeof(float) * n_docs);
         cudaMemcpy(d_doc_lens, lens.data(), sizeof(uint16_t) * n_docs, cudaMemcpyHostToDevice);
 
         std::chrono::high_resolution_clock::time_point d2 = std::chrono::high_resolution_clock::now();
@@ -99,7 +102,10 @@ void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &querys,
 
     // 主线程
     std::chrono::high_resolution_clock::time_point d1 = std::chrono::high_resolution_clock::now();
-    std::vector<float> scores(n_docs);
+    cudaMallocHost(&scores[0], n_docs * sizeof(float));
+    cudaMallocHost(&scores[1], n_docs * sizeof(float));
+    cudaStreamCreate(&kernelStream);
+    cudaStreamCreate(&memcpyStream);
     std::vector<int> s_indices(n_docs);
     cudaMalloc(&d_docs, sizeof(uint16_t) * MAX_DOC_SIZE * n_docs);
     std::chrono::high_resolution_clock::time_point d2 = std::chrono::high_resolution_clock::now();
@@ -115,8 +121,14 @@ void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &querys,
     cudaSetDevice(0);
 
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-    bool needWait = false;
+    uint8_t memcpyWait = 1;
+    uint8_t sortWait = 2;
+    bool step = false;
     std::thread sort_thread;
+    // number: 0 1 2 3 4 5 6 7 8 9
+    // kernel: 0 1 0 1 0 1 0 1
+    // memcpy:   0 1 0 1 0 1 0 1
+    // sorted:     0 1 0 1 0 1 0 1
     for(auto& query : querys) {
 
         // host-to-device
@@ -125,61 +137,167 @@ void doc_query_scoring_gpu_function(std::vector<std::vector<uint16_t>> &querys,
         // launch kernel
         int block = N_THREADS_IN_ONE_BLOCK;
         int grid = (n_docs + block - 1) / block;
-        docQueryScoringCoalescedMemoryAccessSampleKernel<<<grid, block>>>(d_docs, d_doc_lens, n_docs, d_query, query_len, d_scores);
-        cudaDeviceSynchronize();
-
-        if (needWait)
-            sort_thread.join();
-        else
-            needWait = true;
+        docQueryScoringCoalescedMemoryAccessSampleKernel<<<grid, block, 0, kernelStream>>>(d_docs, d_doc_lens, n_docs, d_query, query_len, d_scores[step]);
 
         // device-to-host
-        cudaMemcpy(scores.data(), d_scores, sizeof(float) * n_docs, cudaMemcpyDeviceToHost);
+        if (memcpyWait == 0)
+            cudaMemcpyAsync(scores[!step], d_scores[!step], sizeof(float) * n_docs, cudaMemcpyDeviceToHost, memcpyStream);
+
         // sort scores
-        sort_thread = std::thread([&]() {
-            // 初始下标
-            std::iota(s_indices.begin(), s_indices.end(), 0);
-            // 多线程分块topk排序
-            const int NUM_THREADS = 8;
-            int chunk_size = s_indices.size() / NUM_THREADS;
-            std::vector<std::thread> threads;
-            std::vector<int> merged_results(NUM_THREADS * TOPK);
-            for (int i = 0; i < NUM_THREADS; ++i) {
-                threads.emplace_back([&s_indices, &scores, chunk_size, i, &merged_results]() {
-                    int start = i * chunk_size;
-                    int end = (i == NUM_THREADS - 1) ? s_indices.size() : start + chunk_size;
-                    std::partial_sort(s_indices.begin() + start, s_indices.begin() + start + TOPK, s_indices.begin() + end,
-                                    [&scores](const int& a, const int& b) {
-                                        if (scores[a] != scores[b])
-                                            return scores[a] > scores[b];  // 按照分数降序排序
-                                        return a < b;  // 如果分数相同，按索引从小到大排序
-                                    });
-                    std::copy(s_indices.begin() + start, s_indices.begin() + start + TOPK, merged_results.begin() + i * TOPK);
-                });
-            }
-            for (auto& thread : threads) {
-                thread.join();
-            }
-            // 合并块topk排序
-            std::partial_sort(merged_results.begin(), merged_results.begin() + TOPK, merged_results.end(),
-                            [&scores](const int& a, const int& b) {
-                                if (scores[a] != scores[b])
-                                    return scores[a] > scores[b];  // 按照分数降序排序
-                                return a < b;  // 如果分数相同，按索引从小到大排序
-                            });
-            std::vector<int> s_ans(merged_results.begin(), merged_results.begin() + TOPK);
-            indices.push_back(s_ans);
-        });
+        if (sortWait == 0) {
+            sort_thread = std::thread([&]() {
+                float* cur_scores = scores[step];
+
+                // for (int i = 0; i < n_docs; ++i) s_indices[i] = i;
+                // std::partial_sort(s_indices.begin(), s_indices.begin() + TOPK, s_indices.end(),
+                //                 [&cur_scores](const int& a, const int& b) {
+                //                     if (cur_scores[a] != cur_scores[b])
+                //                         return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                //                     return a < b;  // 如果分数相同，按索引从小到大排序
+                //                 });
+                // std::vector<int> s_ans(s_indices.begin(), s_indices.begin() + TOPK);
+                // indices.push_back(s_ans);
+
+                // 初始下标
+                std::iota(s_indices.begin(), s_indices.end(), 0);
+                // 多线程分块topk排序
+                const int NUM_THREADS = 8;
+                int chunk_size = s_indices.size() / NUM_THREADS;
+                std::vector<std::thread> threads;
+                std::vector<int> merged_results(NUM_THREADS * TOPK);
+                for (int i = 0; i < NUM_THREADS; ++i) {
+                    threads.emplace_back([&s_indices, &cur_scores, chunk_size, i, &merged_results]() {
+                        int start = i * chunk_size;
+                        int end = (i == NUM_THREADS - 1) ? s_indices.size() : start + chunk_size;
+                        std::partial_sort(s_indices.begin() + start, s_indices.begin() + start + TOPK, s_indices.begin() + end,
+                                        [&cur_scores](const int& a, const int& b) {
+                                            if (cur_scores[a] != cur_scores[b])
+                                                return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                                            return a < b;  // 如果分数相同，按索引从小到大排序
+                                        });
+                        std::copy(s_indices.begin() + start, s_indices.begin() + start + TOPK, merged_results.begin() + i * TOPK);
+                    });
+                }
+                for (auto& thread : threads) {
+                    thread.join();
+                }
+                // 合并块topk排序
+                std::partial_sort(merged_results.begin(), merged_results.begin() + TOPK, merged_results.end(),
+                                [&cur_scores](const int& a, const int& b) {
+                                    if (cur_scores[a] != cur_scores[b])
+                                        return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                                    return a < b;  // 如果分数相同，按索引从小到大排序
+                                });
+                std::vector<int> s_ans(merged_results.begin(), merged_results.begin() + TOPK);
+                indices.push_back(s_ans);
+            });
+        }
+
+        // 全部同步
+        cudaStreamSynchronize(kernelStream);
+        cudaStreamSynchronize(memcpyStream);
+        if (sortWait == 0)
+            sort_thread.join();
+
+        // pipeline调度
+        if (memcpyWait > 0) memcpyWait--;
+        if (sortWait > 0) sortWait--;
+
+        // 切换到另一个buffer
+        step = !step;
     }
     
+    cudaMemcpyAsync(scores[!step], d_scores[!step], sizeof(float) * n_docs, cudaMemcpyDeviceToHost, memcpyStream);
+    sort_thread = std::thread([&]() {
+        float* cur_scores = scores[step];
+        // 初始下标
+        std::iota(s_indices.begin(), s_indices.end(), 0);
+        // 多线程分块topk排序
+        const int NUM_THREADS = 8;
+        int chunk_size = s_indices.size() / NUM_THREADS;
+        std::vector<std::thread> threads;
+        std::vector<int> merged_results(NUM_THREADS * TOPK);
+        for (int i = 0; i < NUM_THREADS; ++i) {
+            threads.emplace_back([&s_indices, &cur_scores, chunk_size, i, &merged_results]() {
+                int start = i * chunk_size;
+                int end = (i == NUM_THREADS - 1) ? s_indices.size() : start + chunk_size;
+                std::partial_sort(s_indices.begin() + start, s_indices.begin() + start + TOPK, s_indices.begin() + end,
+                                [&cur_scores](const int& a, const int& b) {
+                                    if (cur_scores[a] != cur_scores[b])
+                                        return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                                    return a < b;  // 如果分数相同，按索引从小到大排序
+                                });
+                std::copy(s_indices.begin() + start, s_indices.begin() + start + TOPK, merged_results.begin() + i * TOPK);
+            });
+        }
+        for (auto& thread : threads) {
+            thread.join();
+        }
+        // 合并块topk排序
+        std::partial_sort(merged_results.begin(), merged_results.begin() + TOPK, merged_results.end(),
+                        [&cur_scores](const int& a, const int& b) {
+                            if (cur_scores[a] != cur_scores[b])
+                                return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                            return a < b;  // 如果分数相同，按索引从小到大排序
+                        });
+        std::vector<int> s_ans(merged_results.begin(), merged_results.begin() + TOPK);
+        indices.push_back(s_ans);
+    });
+
+    sort_thread.join();
+    cudaStreamSynchronize(memcpyStream);
+
+    sort_thread = std::thread([&]() {
+        float* cur_scores = scores[!step];
+        // 初始下标
+        std::iota(s_indices.begin(), s_indices.end(), 0);
+        // 多线程分块topk排序
+        const int NUM_THREADS = 8;
+        int chunk_size = s_indices.size() / NUM_THREADS;
+        std::vector<std::thread> threads;
+        std::vector<int> merged_results(NUM_THREADS * TOPK);
+        for (int i = 0; i < NUM_THREADS; ++i) {
+            threads.emplace_back([&s_indices, &cur_scores, chunk_size, i, &merged_results]() {
+                int start = i * chunk_size;
+                int end = (i == NUM_THREADS - 1) ? s_indices.size() : start + chunk_size;
+                std::partial_sort(s_indices.begin() + start, s_indices.begin() + start + TOPK, s_indices.begin() + end,
+                                [&cur_scores](const int& a, const int& b) {
+                                    if (cur_scores[a] != cur_scores[b])
+                                        return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                                    return a < b;  // 如果分数相同，按索引从小到大排序
+                                });
+                std::copy(s_indices.begin() + start, s_indices.begin() + start + TOPK, merged_results.begin() + i * TOPK);
+            });
+        }
+        for (auto& thread : threads) {
+            thread.join();
+        }
+        // 合并块topk排序
+        std::partial_sort(merged_results.begin(), merged_results.begin() + TOPK, merged_results.end(),
+                        [&cur_scores](const int& a, const int& b) {
+                            if (cur_scores[a] != cur_scores[b])
+                                return cur_scores[a] > cur_scores[b];  // 按照分数降序排序
+                            return a < b;  // 如果分数相同，按索引从小到大排序
+                        });
+        std::vector<int> s_ans(merged_results.begin(), merged_results.begin() + TOPK);
+        indices.push_back(s_ans);
+    });
+
     // deallocation
     cudaFree(d_docs);
     cudaFree(d_query);
-    cudaFree(d_scores);
+    cudaFree(d_scores[0]);
+    cudaFree(d_scores[1]);
     cudaFree(d_doc_lens);
     free(h_docs);
 
+    cudaStreamDestroy(kernelStream);
+    cudaStreamDestroy(memcpyStream);
+
     sort_thread.join();
+
+    cudaFreeHost(scores[0]);
+    cudaFreeHost(scores[1]);
 
     std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();
     std::cout << "[CUDA] preprocess: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << " ms " << std::endl;
